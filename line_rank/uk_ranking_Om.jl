d740451ac1ae98c819373e30ee85aa0b90cd082c
r0b1nu5 <robin.delabays@hevs.ch>
2019-03-11 17:41:36 +0100

    add new files

---
+++ line_rank/uk_ranking_Om.jl
@@ -0,0 +1,96 @@
+using DelimitedFiles
+
+include("centrality.jl")
+include("kf.jl")
+include("kuramoto_pert.jl")
+include("res_dist.jl")
+
+@info("Initialization")
+
+Asp = readdlm("uk_adj_mat.csv",',') .+ 1.0
+n = Int(maximum(Asp))
+m = Int(size(Asp)[1]/2)
+
+A = zeros(Int,n,n)
+for i in 1:330
+	A[Int(Asp[i,1]),Int(Asp[i,2])] = 1.0
+end
+
+L = Array{Float64,2}(diagm(0 => vec(sum(A, dims = 2))) - A)
+
+C = centralities(L)
+Om = res_dist(L)
+
+line_list = Array{Array{Int,1},1}()
+dist = Array{Float64,1}()
+for i in 1:m
+	push!(line_list,Array{Int,1}(vec(Asp[2*i-1,[1,2]])))
+	push!(dist,Om[line_list[end][1],line_list[end][2]])
+end
+
+@info("Computing δKf1")
+
+dKf1 = Array{Float64,1}()
+
+#line_list = line_list[[1,20,40,60,80,100,120,140,160]]
+#m = length(line_list)
+
+for l in line_list
+	b = -L[l[1],l[2]]
+	Ome = Om[l[1],l[2]]
+	if abs(b-Ome) < 1e-10
+		push!(dKf1,Inf)
+	else
+		push!(dKf1,b*Ome/(1-b*Ome))
+	end
+end
+
+ranked_line_idx = Array{Int,1}(vec(sortslices([dKf1 1:m],dims=1)[:,2]))
+ranked_lines = line_list[ranked_line_idx]
+
+#=
+@info("Computing peformance measure(s)")
+
+C1s = Array{Float64,1}()
+C2s = Array{Float64,1}()
+
+for l in line_list
+	Ltemp = copy(L)
+	Ltemp[l[1],l[2]] += 1
+	Ltemp[l[2],l[1]] += 1
+	Ltemp[l[1],l[1]] -= 1
+	Ltemp[l[2],l[2]] -= 1
+	
+	C1,C2 = kuramoto_pert(Ltemp,zeros(n),zeros(n),("box",[.3,10,8]),1e-8,.1,10000)
+	push!(C1s,C1)
+	push!(C2s,C2)
+end
+
+C1_rank_idx = Array{Int,1}(vec(sortslices([C1s 1:m],dims=1)[:,2]))
+C1_ranking = line_list[C1_rank_idx]
+C2_rank_idx = Array{Int,1}(vec(sortslices([C2s 1:m],dims=1)[:,2]))
+C2_ranking = line_list[C2_rank_idx]
+
+x = vec(sortslices([ranked_line_idx 1:m],dims=1)[:,2])
+y1 = vec(sortslices([C1_rank_idx 1:m],dims=1)[:,2])
+figure()
+PyPlot.plot(x,y1,"o")
+xlabel("δKf1")
+ylabel("C1")
+
+y2 = vec(sortslices([C2_rank_idx 1:m],dims=1)[:,2])
+figure()
+PyPlot.plot(x,y2,"o")
+xlabel("δKf1")
+ylabel("C2")
+
+figure()
+PyPlot.plot(y1,y2,"o")
+xlabel("C1")
+ylabel("C2")
+
+
+=#
+
+
+
---
+++ line_rank/uk_ranking_perf.jl
@@ -0,0 +1,178 @@
+using DelimitedFiles,Statistics,LinearAlgebra,PyPlot
+
+include("kuramoto.jl")
+include("L2B.jl")
+include("gen_idx.jl")
+
+lin_or_sin = "sin"
+ntw = "uk"
+
+tau = 0.25 # duration of the line contingency
+
+eps = 1e-6
+max_iter = 100000
+h = 0.025
+
+if tau < h
+	@warn("τ must be larger than h")
+end
+
+# Choose network
+if ntw == "uk"
+	Asp = readdlm("uk_adj_mat.csv",',') .+ 1
+end
+
+# Initialize parameters
+n = Int(maximum(Asp))
+m = Int(size(Asp)[1]/2)
+
+A = zeros(n,n)
+for i in 1:2*m
+	A[Int(Asp[i,1]),Int(Asp[i,2])] = 1.
+end
+L = Array{Float64,2}(diagm(0 => vec(sum(A, dims=2))) - A)
+
+line_list = Array{Array{Int,1},1}()
+for i in 1:m
+	push!(line_list,Array{Int,1}(vec(Asp[2*i-1,[1,2]])))
+end
+
+mm = .2*ones(n)
+dd = .1*ones(n)
+
+P = zeros(n)
+P[gen_idx] = .2*ones(length(gen_idx))
+P .-= mean(P)
+
+th0 = zeros(n)
+omeg0 = zeros(n)
+x0 = [th0;omeg0]
+
+# Find fixed point
+if lin_or_sin == "sin"
+	xs1,dxs1 = kuramoto2(L,mm,dd,P,x0[1:n],x0[(n+1):(2*n)],true,max_iter,eps,h)
+elseif lin_or_sin == "lin"
+	xs1,dxs1 = kuramoto2_lin(L,mm,dd,P,x0[1:n],x0[(n+1):(2*n)],true,max_iter,eps,h)
+end
+x1 = vec(xs1[:,end])
+
+
+P1s = Array{Float64,1}()
+P2s = Array{Float64,1}()
+
+# Simulate line contingency for each line and compute performance measures
+for l in line_list
+	@info(l)
+	global P1s,P2s
+	Ltemp = copy(L)
+	Ltemp[l,l] += [-1 1;1 -1]
+	ls = eigvals(Ltemp)
+	if sum(abs.(eigvals(Ltemp)) .< 1e-8) > 1
+		push!(P1s,Inf)
+		push!(P2s,Inf)
+	else
+		if lin_or_sin == "sin"
+			xs2,dxs2 = kuramoto2(Ltemp,mm,dd,P,x1[1:n],x1[(n+1):(2*n)],true,floor(Int,tau/h),eps,h)
+			xs3,dxs3 = kuramoto2(L,mm,dd,P,vec(xs2[1:n,end]),vec(xs2[(n+1):(2*n),end]),true,max_iter,eps,h)
+		elseif lin_or_sin == "lin"
+			xs2,dxs2 = kuramoto2_lin(Ltemp,mm,dd,P,x1[1:n],x1[(n+1):(2*n)],true,floor(Int,tau/h),eps,h)
+			xs3,dxs3 = kuramoto2_lin(L,mm,dd,P,vec(xs2[1:n,end]),vec(xs2[(n+1):(2*n),end]),true,max_iter,eps,h)
+		end
+		
+		ths = [xs2[1:n,:] xs3[1:n,:]]
+		omegs = [xs2[(n+1):(2*n),:] xs3[(n+1):(2*n),:]]
+		
+		if lin_or_sin == "sin"		
+			push!(P1s,h*sum((ths-repeat(x1[1:n],outer=(1,size(ths)[2]))).^2)/abs(1-cos(x1[l[1]]-x1[l[2]])))
+			push!(P2s,h*sum(omegs.^2)/abs(1-cos(x1[l[1]]-x1[l[2]])))
+		elseif lin_or_sin == "lin"
+			push!(P1s,h*sum((ths-repeat(x1[1:n],outer=(1,size(ths)[2]))).^2)/(x1[l[1]]-x1[l[2]])^2)
+			push!(P2s,h*sum(omegs.^2)/(x1[l[1]]-x1[l[2]])^2)
+		end
+			
+	end
+end
+
+# Computing resistance distances
+Om = res_dist(L)
+
+line_list = Array{Array{Int,1},1}()
+dist = Array{Float64,1}()
+for i in 1:m
+	push!(line_list,Array{Int,1}(vec(Asp[2*i-1,[1,2]])))
+	push!(dist,Om[line_list[end][1],line_list[end][2]])
+end
+
+@info("Computing δKf1")
+
+dKf1 = Array{Float64,1}()
+
+for l in line_list
+	b = -L[l[1],l[2]]
+	Ome = Om[l[1],l[2]]
+	if abs(b-Ome) < 1e-10
+		push!(dKf1,Inf)
+	else
+		push!(dKf1,b*Ome/(1-b*Ome))
+	end
+end
+
+ranked_line_idx = Array{Int,1}(vec(sortslices([dKf1 1:m],dims=1)[:,2]))
+ranked_lines = line_list[ranked_line_idx]
+
+
+# Dealing with tree-like parts
+idx = Array{Int64,1}()
+for i in 1:m
+	if !(isinf(dKf1[i]) || isinf(P1s[i]) || isinf(P2s[i]))
+		push!(idx,i)
+	end
+end
+
+# Compute correlations
+x = dKf1[idx]
+y = P1s[idx]
+z = P2s[idx]
+t = dist[idx]
+
+r1 = sum((x .- mean(x)).*(y .- mean(y)))/(sqrt(sum((x .- mean(x)).^2))*sqrt(sum((y .- mean(y)).^2)))
+r2 = sum((x .- mean(x)).*(z .- mean(z)))/(sqrt(sum((x .- mean(x)).^2))*sqrt(sum((z .- mean(z)).^2)))
+r3 = sum((z .- mean(z)).*(y .- mean(y)))/(sqrt(sum((z .- mean(z)).^2))*sqrt(sum((y .- mean(y)).^2)))
+r4 = sum((t .- mean(t)).*(y .- mean(y)))/(sqrt(sum((t .- mean(t)).^2))*sqrt(sum((y .- mean(y)).^2)))
+r5 = sum((t .- mean(t)).*(z .- mean(z)))/(sqrt(sum((t .- mean(t)).^2))*sqrt(sum((z .- mean(z)).^2)))
+
+# Plot
+figure(lin_or_sin,(15,8))
+
+PyPlot.subplot(1,5,1)
+PyPlot.plot(dKf1,P1s,"ob")
+xlabel("δKf1")
+ylabel("P1")
+title("r = $(round(r1,digits=4))")
+
+PyPlot.subplot(1,5,2)
+PyPlot.plot(dKf1,P2s,"or")
+xlabel("δKf1")
+ylabel("P2")
+title("r = $(round(r2,digits=4))")
+
+PyPlot.subplot(1,5,3)
+PyPlot.plot(P1s,P2s,"og")
+xlabel("P1")
+ylabel("P2")
+title("r = $(round(r3,digits=4))")
+
+PyPlot.subplot(1,5,4)
+PyPlot.plot(dist,P1s,"oy")
+xlabel("Ω")
+ylabel("P1")
+title("r = $(round(r4,digits=4))")
+
+PyPlot.subplot(1,5,5)
+PyPlot.plot(dist,P2s,"om")
+xlabel("Ω")
+ylabel("P2")
+title("r = $(round(r5,digits=4))")
+
+
+
