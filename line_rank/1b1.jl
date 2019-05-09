15bb38fe95fe9eb57369ce76e52b0068de6e1850
r0b1nu5 <robin.delabays@hevs.ch>
2019-05-09 13:30:06 +0200

    line ranking: correction in Omega measure and implement NR

--- line_rank/1b1.jl
+++ line_rank/1b1.jl
@@ -13,18 +13,19 @@ include("sync.jl")
 # ranking_type: ranking used to remove the lines ("init": at each step, removes the most central line according to the measure in the initial network. "updaed": same but updates the ranking after removing each line. "random": removes lines randomly. "tini": same as "init" but removes the least central line. "detadpu": same as "updated" but removes the least central line.)
 # ranking_measure: measure used to make the ranking ("Omega": resistance distance, "load": uses the load on the line, "Omega+load": uses the resistance distance rescaled by the load on the line.)
 
-function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Float64,M::Array{Float64,1},D::Array{Float64,1},eps::Float64=1e-6,max_iter::Int64=100000,h::Float64=.1)
+function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Float64,M::Array{Float64,1},D::Array{Float64,1},max_iter::Int64=50,eps::Float64=1e-6,h::Float64=.1)
 	
 	Asp = readdlm(ntw*"_adj_mat.csv",',')
-	
-	n = round(Int,maximum(Asp[:,1]))
-	m = Int(size(Asp)[1]/2)
+	Lsp = readdlm(ntw*"_lap_mat.csv",',')
+	Bsp = readdlm(ntw*"_inc_mat.csv",',')
 	
 	A = sparse(Array{Int,1}(Asp[:,1]),Array{Int,1}(Asp[:,2]),vec(Asp[:,3]))
-	L = spdiagm(0 => vec(sum(A,dims=2))) - A
-	
-	Om = res_dist(L)
-	
+	L = sparse(Array{Int,1}(Lsp[:,1]),Array{Int,1}(Lsp[:,2]),vec(Lsp[:,3]))
+	B = sparse(Array{Int,1}(Bsp[:,1]),Array{Int,1}(Bsp[:,2]),vec(Bsp[:,3]))
+	w = Bsp[2*(1:Int(size(Bsp)[1]/2)),4]
+
+	n,m = size(B)
+		
 	P = P0*vec(readdlm("P_"*ntw*".csv",','))
 	P .-= mean(P)
 	
@@ -34,22 +35,39 @@ function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Fl
 	else
 		x1 = vec(readdlm("sync_states/"*ntw*"_sync_$P0.csv",','))
 	end
-	dx1 = x1[1:n]*ones(1,n) - ones(n)*transpose(x1[1:n])
-	load = L.*dx1
-	load_rate = abs.(dx1./(pi/2))
 	
 	ll = Array{Tuple{Int64,Int64},1}()
 	mes = Array{Float64,1}()
-	for i in 1:m
-		l = (Int64(Asp[2*i-1,1]),Int(Asp[2*i-1,2]))
-		push!(ll,l)
-		if ranking_measure == "Omega"
-			push!(mes,Om[l[1],l[2]])
-		elseif ranking_measure == "load"
+	
+	if ranking_measure == "Omega"
+		Om = res_dist(L)
+		for i in 1:m
+			l = (Int(Bsp[2*i-1,1]),Int(Bsp[2*i,1]))
+			push!(ll,l)
+			push!(mes,L[l[1],l[2]]*Om[l[1],l[2]])
+		end
+	elseif ranking_measure == "load"
+		dx1 = x1[1:n]*ones(1,n) - ones(n)*transpose(x1[1:n])
+		load = L.*dx1
+		load_rate = abs.(dx1./(pi/2))
+		for i in 1:m
+			l = (Int(Bsp[2*i-1,1]),Int(Bsp[2*i,1]))
+			push!(ll,l)
 			push!(mes,load_rate[l[1],l[2]])
-		elseif ranking_measure == "Omega+load"
+		end
+	elseif ranking_measure == "Omega+load"
+		Om = res_dist(L)
+		dx1 = x1[1:n]*ones(1,n) - ones(n)*transpose(x1[1:n])
+		load = L.*dx1
+		for i in 1:m
+			l = (Int(Bsp[2*i-1,1]),Int(Bsp[2*i,1]))
+			push!(ll,l)
 			push!(mes,Om[l[1],l[2]]*load[l[1],l[2]]^2)
-		else
+		end
+	else
+		for i in 1:m
+			l = (Int(Bsp[2*i-1,1]),Int(Bsp[2*i,1]))
+			push!(ll,l)
 			@info "$(now()) -- "*ntw*": No clear ranking strategy..."
 		end
 	end
@@ -70,6 +88,8 @@ function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Fl
 	end
 	cuts = Array{Array{Int64,1},1}([cut,])
 	Lr = copy(L)
+	Br = copy(B)
+	wr = copy(w)
 	
 	count = 0
 	
@@ -92,7 +112,7 @@ function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Fl
 		elseif ranking_type == "updated"
 			used_rank = rank
 		elseif ranking_type == "random"
-			used_rank = rand(1:m,10000)
+			used_rank = Array{Int64,1}(sortslices([rand(length(rank)) 1:m],dims=1)[:,2])
 		elseif ranking_type == "tini"
 			used_rank = rank0[end:-1:1]
 		elseif ranking_type == "detadpu"
@@ -115,28 +135,42 @@ function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Fl
 			run = false
 		else
 			Lr = copy(Lt)
+			Br,wr = rm_line_inc(Br,wr,ll[i])
 			push!(rmvd,i)
 			push!(cuts,cut)
 			l = ll[i]
-			Om = res_dist(Lr)
 			
-			xs,dxs,n_iter = kuramoto2(Lr,M,D,P,x1[1:n],x1[(n+1):(2*n)])
+			xs,n_iter = NR_kuramoto(Br,wr,P)
+#			xs,dxs,n_iter = kuramoto2(Lr,M,D,P,x1[1:n],x1[(n+1):(2*n)])
+			stable = isstable(Lr,th)
 			
-			ddx = xs[1:n]*ones(1,n) - ones(n)*transpose(xs[1:n])
-			
-			load = Lr.*ddx
-			load_rate = abs.(ddx./(pi/2))
 			
 			mes = Array{Float64,1}()
-			for i in setdiff(1:m,rmvd)
-				l = ll[i]
-				if ranking_measure == "Omega"
-					push!(mes,Om[l[1],l[2]])
-				elseif ranking_measure == "load"
+			if ranking_measure == "Omega"
+				Om = res_dist(Lr)
+				for i in setdiff(1:m,rmvd)
+					l = ll[i]
+					push!(mes,Lr[l[1],l[2]]*Om[l[1],l[2]])
+				end
+			elseif ranking_measure == "load"
+				ddx = xs[1:n]*ones(1,n) - ones(n)*transpose(xs[1:n])	
+				load = Lr.*ddx
+				load_rate = abs.(ddx./(pi/2))
+				for i in setdiff(1:m,rmvd)
+					l = ll[i]
 					push!(mes,load_rate[l[1],l[2]])
-				elseif ranking_measure == "Omega+load"
+				end
+			elseif ranking_measure == "Omega+load"
+				Om = res_dist(Lr)
+				ddx = xs[1:n]*ones(1,n) - ones(n)*transpose(xs[1:n])	
+				load = Lr.*ddx
+				for i in setdiff(1:m,rmvd)
+					l = ll[i]
 					push!(mes,Om[l[1],l[2]]*load[l[1],l[2]]^2)
-				else 
+				end
+			else 
+				for i in setdiff(1:m,rmvd)
+					l = ll[i]
 					@info "$(now()) -- "*ntw*": No clear ranking strategy..."
 				end
 			end
@@ -146,6 +180,9 @@ function rmv_1b1(ntw::String,ranking_type::String,ranking_measure::String,P0::Fl
 			if n_iter >= max_iter
 				run = false
 				@info "$(now()) -- "*ntw*": No sync anymore => EXIT"
+			elseif !stable
+				run = false
+				@info "$(now()) -- "*ntw*": Solution is not stable => EXIT"
 			end
 		end
 	end
--- line_rank/rm_line.jl
+++ line_rank/rm_line.jl
@@ -11,5 +11,28 @@ function rm_line(L::Union{Array{Float64,2},SparseMatrixCSC{Float64,Int}},l::Tupl
 	return nL
 end
 
+function rm_line_inc(B::Union{Array{Float64,2},SparseMatrixCSC{Float64,Int}},w::Array{Float64,1},l::Tuple{Int,Int})
+	n,m = size(B)
+	
+	x,y,z = findnz(B)
+	idx = setdiff(((x[2*(1:m).-1] .== l[1]).*(x[2*(1:m)] .== l[2]) + (x[2*(1:m).-1] .== l[2]).*(x[2*(1:m)] .== l[1])).*(1:m),[0,])
+	
+	if length(idx) == 0
+		@info "The line does not exist..."
+	elseif length(idx) > 1
+		@info "Multiple lines ???"
+		Br = B[:,setdiff(1:m,idx)]
+		wr = w[setdiff(1:m,idx)]
+	else
+		Br = B[:,setdiff(1:m,idx)]
+		wr = w[setdiff(1:m,idx)]
+	end
+		
+	return Br,wr
+end
+
+
+
+
 
 
