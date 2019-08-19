6e646c8c4a99e561f1979ed93504d1b784a9adae
r0b1nu5 <robin.delabays@hevs.ch>
2019-08-19 15:16:45 -0600

    better ?

--- lanl/objective.jl
+++ lanl/objective.jl
@@ -6,7 +6,7 @@ function objective1(Xs::Array{Float64,2}, Lm::Array{Float64,2}, dm::Array{Float6
 
 	err = Xs[n+1:2*n,2:end] - Xs[n+1:2*n,1:end-1] - dt * (-Lm*Xs[1:n,1:end-1] - diagm(0 => dm)*Xs[n+1:2*n,1:end-1] + repeat(a,1,T-1).*cos.(2*pi*dt*f*Array(1:T-1)' .+ repeat(phi,1,T-1)))
 	
-	return sum(err.^2) - l1 * sum(Lm .* (1 .- diagm(0 => ones(n))) + l2 * sum(a))
+	return sum(err.^2) - l1 * sum(Lm .* (1 .- diagm(0 => ones(n)))) + l2 * sum(a)
 end
 
 
