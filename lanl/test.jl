86a0e72a4e5633fb5707999501a4f764901f634b
r0b1nu5 <robin.delabays@hevs.ch>
2019-08-27 16:02:36 -0600

    ...

--- lanl/test.jl
+++ lanl/test.jl
@@ -1,4 +1,4 @@
-using JuMP, Ipopt, PyPlot
+using JuMP, Ipopt, PyPlot, Statistics
 
 include("locate_forced.jl")
 
@@ -289,7 +289,7 @@ function test_w_forcing3(L::Array{Float64,2}, d::Array{Float64,1}, aa::Array{Flo
 	L0 = -Ah[n+1:2*n,1:n]/dt
 	
 	f0 = mean(find_freq_autocorr(Xs,dt))
-
+f0 /= 2
 	a0 = maximum(Xs[n+1:2*n,:])*sqrt(n)/f0
 
 	@variable(system_id, Lm[i = 1:n, j = 1:n])
@@ -308,31 +308,78 @@ function test_w_forcing3(L::Array{Float64,2}, d::Array{Float64,1}, aa::Array{Flo
                         @constraint(system_id, Lm[j,i] <= 1e-5)
                 end
         end
-        
+# =#
+# #=
         for i in 1:n
                 @constraint(system_id, sum(Lm[i,:]) == 0)
         end
 # =#
 
-	@NLexpression(system_id, err[i = 1:n, t = 1:Dt-1, j = 1:n_bin], Xs[n+i,t+1] - Xs[n+i,t] - dt * (sum(-Lm[i,k]*Xs[k,t] for k = 1:n) - dm[i]*Xs[n+i,t] + a[i]*cos(2*pi*f[i]*t*dt + phi[i,j])))
+	@NLexpression(system_id, err[i = 1:n, t = 1:Dt-1, j = 1:n_bin], Xs[n+i,(j-1)*Dt + t + 1] - Xs[n+i,(j-1)*Dt + t] - dt * (sum(-Lm[i,k]*Xs[k,(j-1)*Dt + t] for k = 1:n) - dm[i]*Xs[n+i,(j-1)*Dt + t] + a[i]*cos(2*pi*f[i]*((j-1)*Dt + t)*dt + phi[i,j])))
 #	@NLexpression(system_id, err[i = 1:n, t = 1:T-1], Xs[n+i,t+1] - Xs[n+i,t] - dt * (sum(-Lm[i,k]*Xs[k,t] for k = 1:n) - dm[i]*Xs[n+i,t]))
 
 #@NLobjective(system_id, Min, 1/T*sum(err[i,t,j]^2 for i = 1:n for t = 1:Dt-1 for j = 1:n_bin) + l1 * sum(abs(Lm[i,j]) + abs(Lm[j,i]) for i = 1:n-1 for j = i+1:n) + l2 * sum(abs(a[i]) for i in 1:n)) 
 @NLobjective(system_id, Min, 1/T*sum(err[i,t,j]^2 for i = 1:n for t = 1:Dt-1 for j = 1:n_bin)) 
         
         optimize!(system_id)
+        
+	return (Lm = value.(Lm), dm = value.(dm), a = value.(a), f = value.(f), phi = value.(phi))
+end
+
+function test_w_forcing3_l0(id::Int64, L::Array{Float64,2}, d::Array{Float64,1}, a0::Array{Float64,1}, f0::Array{Float64,1}, p0::Array{Float64,1}, Xs::Array{Float64,2}, dt::Float64, Dt::Int64, mu::Float64=1e-1, bp::Float64=1e-1)
+	nn,T = size(Xs)
+	n = Int.(nn/2)
+	n_bin = floor(Int,T/Dt)
+
+	system_id = Model(with_optimizer(Ipopt.Optimizer, mu_init = mu, bound_push = bp))
+        
+	Ah,x = system_identification_correl(Xs,dt)
+	L0 = -Ah[n+1:2*n]/dt
+
+	f0 = median(find_freq_autocorr(Xs,dt))
+
+	a0 = maximum(abs.(Xs[n+1:2*n,:])*sqrt(n)/f0)
+
+	@variable(system_id, Lm[i = 1:n, j = 1:n])
+	set_start_value.(Lm,L0)
+	@variable(system_id, dm[i = 1:n])
+	@variable(system_id, a)
+	set_start_value(a,a0)
+	@variable(system_id, f)
+	set_start_value(f,f0)
+	@variable(system_id, phi[j = 1:n_bin])
+        
+	for i in 1:n-1
+                for j in i+1:n
+                        @constraint(system_id, Lm[i,j] <= 0)
+                        @constraint(system_id, Lm[j,i] <= 0)
+                end
+        end
+        
+        for i in 1:n
+                @constraint(system_id, sum(Lm[i,:]) == 0)
+        end
+
+	@NLexpression(system_id, err[i = [1:id-1;id+1:n], t = 1:Dt-1, j = 1:n_bin], Xs[n+i,(j-1)*Dt + t + 1] - Xs[n+i,(j-1)*Dt + t] - dt * (sum(-Lm[i,k]*Xs[k,(j-1)*Dt + t] for k = 1:n) - dm[i]*Xs[n+i,(j-1)*Dt + t]))
+	@NLexpression(system_id, erri[t = 1:Dt-1, j = 1:n_bin], Xs[n+id,(j-1)*Dt + t + 1] - Xs[n+id,(j-1)*Dt + t] - dt * (sum(-Lm[id,k]*Xs[k,(j-1)*Dt + t] for k = 1:n) - dm[id]*Xs[n+id,(j-1)*Dt + t] + a*cos(2*pi*f*dt*((j-1)*Dt + t) + phi[j])))
+#	@NLexpression(system_id, err[i = 1:n, t = 1:T-1], Xs[n+i,t+1] - Xs[n+i,t] - dt * (sum(-Lm[i,k]*Xs[k,t] for k = 1:n) - dm[i]*Xs[n+i,t]))
+
+	@NLobjective(system_id, Min, 1/T*sum(err[i,t,j]^2 for i = [1:id-1;id+1:n] for t = 1:Dt-1 for j = 1:n_bin) + sum(erri[t,j]^2 for t = 1:Dt-1 for j = 1:n_bin)) 
+        
+        optimize!(system_id)
+
 #=
 	reLm = abs.(L - value.(Lm))./abs.(L)
 	redm = abs.(d - value.(dm))./abs.(d)
-	rea = abs.(aa - value.(a))./abs.(a0)
-	ref = abs.(ff - value.(f))./abs.(f0)
-	rep = abs.(pp - value.(phi))./abs.(p0)
+	rea = abs.(a0 .- value.(a))./abs.(a0)
+	ref = abs.(f0 .- value.(f))./abs.(f0)
+	rep = abs.(p0 .- value.(phi))./abs.(p0)
 
 	aeLm = abs.(L - value.(Lm))
 	aedm = abs.(d - value.(dm))
-	aea = abs.(aa - value.(a))
-	aef = abs.(ff - value.(f))
-	aep = abs.(pp - value.(phi))
+	aea = abs.(a0 .- value.(a))
+	aef = abs.(f0 .- value.(f))
+	aep = abs.(p0 .- value.(phi))
 	
 	figure()
 	subplot(211)
@@ -352,9 +399,9 @@ function test_w_forcing3(L::Array{Float64,2}, d::Array{Float64,1}, aa::Array{Flo
 	title("Absolute error")
 	legend()
 =#
+
         return (Lm = value.(Lm), dm = value.(dm), a = value.(a), f = value.(f), phi = value.(phi))
 end
-
 function test_w_forcing_warm_start(L::Array{Float64,2}, d::Array{Float64,1}, a0::Array{Float64,1}, f0::Array{Float64,1}, p0::Array{Float64,1}, Xs::Array{Float64,2}, dt::Float64, L0::Array{Float64,2}, d0::Array{Float64,1}, l1::Float64=0., l2::Float64=0., mu::Float64=1e-1)
 	nn,T = size(Xs)
 	n = Int.(nn/2)
