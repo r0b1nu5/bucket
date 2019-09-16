7feb3937c0db239690adf7c6b88a09a1e2d464d7
r0b1nu5 <robin.delabays@hevs.ch>
2019-09-16 16:02:03 +0200

    vote 1.0

--- kbc/kbc.jl
+++ kbc/kbc.jl
@@ -40,6 +40,55 @@ function kbc(w0::Array{Float64,1}, Dth::Float64, th0::Array{Float64,1}, eps::Flo
 	return ths
 end
 
+function kbc_longrun(w0::Array{Float64,1}, Dth::Float64, th0::Array{Float64,1}, max_iter::Int64, eps::Float64=1e-8, h::Float64=.001)
+	n = length(th0)
+
+	B = inc_ata(n)
+
+	ths = Array{Float64,2}(undef,n,0)
+	th1 = zeros(n)
+	th2 = th0
+
+	its = Array{Int64,1}()
+
+	err = 1000.
+	c = 0
+
+	while err > eps && c < max_iter
+		c += 1
+		if c%10000 == 0
+			@info "$(now()) -- iter = $c, err = $err"
+		end
+
+		dth = mod.(B'*th2 .+ pi,2pi) .- pi
+		test = diagm(0 => (abs.(dth) .<= Dth))
+		Bt = B*test
+
+		k1 = w0 - Bt*sin.(dth)./n
+		k2 = w0 - Bt*sin.(dth + h/2*B'*k1)./n
+		k3 = w0 - Bt*sin.(dth + h/2*B'*k2)./n
+		k4 = w0 - Bt*sin.(dth + h*B'*k3)./n
+
+		thd = (k1 + 2*k2 + 2*k3 + k4)/6
+
+		th1 = copy(th2)
+		th2 += h*thd
+		
+		x = mod(th1[1] + pi,2pi) - pi
+		y = mod(th2[1] + pi,2pi) - pi
+
+		if x*y < 0 && abs(x) < 1.
+			ths = [ths th2]
+			push!(its,c)
+		end
+
+		err = maximum(abs.(h*thd))
+	end
+
+	return ths,its
+end
+
+
 function get_lap(th::Array{Float64,1}, Dth::Float64)
 	n = length(th)
 
---
+++ vote/scripts.jl
@@ -0,0 +1,91 @@
+using Distributions, PyPlot, Statistics, LinearAlgebra
+
+function influence_effort_rand(n::Int64, eps::Float64, Di::Distribution)
+	x0 = rand(Di,n)
+	
+	A = Float64.((0 .< (repeat(x0,1,n) - repeat(x0',n,1)) .< eps))
+	L = diagm(0 => vec(sum(A,dims=1))) - A
+	
+	xr = zeros(n)
+	x = consensus(L,x0,zeros(n),ones(n),ones(n))
+	o0,p0,n0 = outcome(x)
+
+	if o0 > 0.
+		x = x[n:-1:1]
+		o0,p0,n0 = outcome(x)
+	end
+	
+	o1 = o0
+	ids = Array(1:n)
+
+	while o1 < 0.
+		j = rand(1:length(ids))
+		i = ids[j]
+		xr[i] = 1.
+		ids = [ids[1:j-1];ids[j+1:length(ids)]]
+
+		x = consensus(L,x0,xr,ones(n),ones(n))
+		o1,p1,n1 = outcome(x)
+	end
+
+	return sum(xr)
+end
+
+function loops(n_run::Int64, n::Int64, epss::Array{Float64,1}, Di::Distribution)
+	m = length(epss)
+	
+	effort = zeros(n_run,m)
+
+	for j in 1:m
+		@info "j = $j/$m"
+		for i in 1:n_run
+			effort[i,j] = influence_effort_rand(n,epss[j],Di)
+		end
+	end
+
+	return effort
+end
+
+function quants(effort::Array{Float64,2})
+	n,m = size(effort)
+	
+	q0 = Array{Float64,1}()
+	q25 = Array{Float64,1}()
+	q50 = Array{Float64,1}()
+	q75 = Array{Float64,1}()
+	q00 = Array{Float64,1}()
+
+	for j in 1:m
+		push!(q0, quantile(effort[:,j], 0.))
+		push!(q25, quantile(effort[:,j], 0.25))
+		push!(q50, quantile(effort[:,j], 0.5))
+		push!(q75, quantile(effort[:,j], 0.75))
+		push!(q00, quantile(effort[:,j], 1.))
+	end
+
+	return q0,q25,q50,q75,q00
+end
+
+function consensus(L::Array{Float64,2}, x0::Array{Float64,1}, xr::Array{Float64,1}, a::Array{Float64,1}, f::Array{Float64,1})
+	return inv(L + diagm(0 => a))*(diagm(0 => a)*x0 + diagm(0 => f)*xr)
+end
+
+function outcome(x::Array{Float64,1})
+	return sum(sign.(x)), sum(x .> 0.), sum(x .< 0.)
+end
+
+function plot_quants(q0::Array{Float64,1}, q25::Array{Float64,1}, q50::Array{Float64,1}, q75::Array{Float64,1}, q00::Array{Float64,1}, epss::Array{Float64,1}, colo::String="C0") 
+	for i in 1:length(epss)
+		PyPlot.plot(epss[i]*[1,1],[q25[i],q75[i]],color=colo)
+	end
+
+	PyPlot.plot(epss,q0,"x",color=colo)
+	PyPlot.plot(epss,q00,"x",color=colo)
+	PyPlot.plot(epss,q50,"o",color=colo)
+end
+
+
+
+
+
+
