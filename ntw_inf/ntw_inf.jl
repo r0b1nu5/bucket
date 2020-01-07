37c324764fbb336cc673a65cabe67c92cec79f57
r0b1nu5 <robin.delabays@hevs.ch>
2020-01-07 13:30:59 +0100

    just run it!

--- ntw_inf/ntw_inf.jl
+++ ntw_inf/ntw_inf.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra
+using LinearAlgebra, Statistics
 
 function ntw_inf_noise(ths::Array{Float64,2}, dP0::Float64)
 	n,T = size(ths)
@@ -14,8 +14,15 @@ function ntw_inf_noise(ths::Array{Float64,2}, dP0::Float64)
 	return Lh
 end
 
+# th_ij: angle time series at node j when node i is submitted to a sine perturbation.
 
-function ntw_inf_sine(ths::Array{Float64,2}
+function ntw_inf_sine(th_ij::Array{Float64,1}, n::Int64, a0::Float64, w0::Float64, p0::Float64, h::Float64=.1)
+	Lijds = (th_ij - a0*sin.(w0*h*Array(0:(length(th_ij)-1)) .+ p0)./(n*w0))./(a0*cos(w0*h*Array(0:(length(th_ij)-1)) .+ p0))
+
+	Lijdh = mean(Lijds)
+
+	return Lijdh
+end
 
 
 
---
+++ ntw_inf/run.jl
@@ -0,0 +1,15 @@
+using PyPlot
+
+
+# generate time series with noise
+#
+# analyze time series with noise
+#
+# generate time series with sine probing
+#
+# analyze time series with sine probing
+
+
+
+
+
