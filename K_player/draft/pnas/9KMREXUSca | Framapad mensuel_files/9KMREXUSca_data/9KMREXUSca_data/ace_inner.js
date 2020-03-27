61b5e3798223ac3eef380112181fb8b04a38217a
r0b1nu5 <robin.delabays@hevs.ch>
2020-03-27 11:43:11 +0100

    update K-player

--- K_player/draft/pnas/9KMREXUSca | Framapad mensuel_files/9KMREXUSca_data/9KMREXUSca_data/ace_inner.js
+++
@@ -1,1 +0,0 @@
-$(document).bind("keyup",function(e){if(!parent.parent.pad.myUserInfo.name){clearTimeout(n);var n=setTimeout(function(){parent.parent.$("#users").is(":visible")||parent.parent.$("#users").show()},5e3)}});
--- K_player/draft/pnas/9KMREXUSca | Framapad mensuel_files/main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var e=new RegExp(/.*\/p\/[^\/]+/).exec(document.location.pathname)||clientVars.padId;document.location.href.replace(document.location.pathname,e);$("#exportmarkdowna").attr("href",e+"/export/markdown")});
