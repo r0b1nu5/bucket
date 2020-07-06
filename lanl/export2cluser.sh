da3958e19e9d0abacac70891a77d9740c598d29a
r0b1nu5 <robin.delabays@hevs.ch>
2020-07-06 16:38:30 +0200

    just started

---
+++ lanl/export2cluser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#scp-hades data_melvyn/*.csv cluster@10.61.65.42:/home/cluster/bucket/lanl/data_melvyn/
+scp-hades data_PEN/Xs*.csv cluster@10.61.65.42:/home/cluster/bucket/lanl/data_PEN/
+
+
---
+++ lanl/retreive_new.jl
@@ -0,0 +1,19 @@
+using DelimitedFiles, PyPlot
+
+ntw = "ieee57_1"
+lmin,lmax,dl = (,,)
+kmin,kmax,dk = (,,)
+
+Ls = Array{Float64,2}()
+
+for 
+	
+
+
+end
+
+
+
+
+
+
