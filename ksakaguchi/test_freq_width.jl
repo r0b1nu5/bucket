87d32cf26f5a80c742d8fe2ed745a53f6e933ec0
r0b1nu5 <robindelabays@ucsb.edu>
2021-03-26 17:16:44 -0700

    apparently, multiple sols per winding cell... to investigate!

--- ksakaguchi/test_freq_width.jl
+++ ksakaguchi/test_freq_width.jl
@@ -22,16 +22,22 @@ L = cyqle(n)
 αs = [1.,]
 
 for α in αs
-	xxx = freq_width(L,ω,θ0,α,Array(1:n))
+	xxx = freq_width(L,ω,θ0,α,Array(1:n),true)
 	global β0 = [β0 [xxx[1],xxx[2]]]
-	xxx = freq_width(L,ω,θ1,α,Array(1:n))
+	xxx = freq_width(L,ω,θ1,α,Array(1:n),true)
 	global β1 = [β1 [xxx[1],xxx[2]]]
 end
 
+ #=
 writedlm("temp_data/αs_$(run).csv",αs,',')
 writedlm("temp_data/β0min_$(run).csv",β0[1,:],',')
 writedlm("temp_data/β0max_$(run).csv",β0[2,:],',')
 writedlm("temp_data/β1min_$(run).csv",β1[1,:],',')
 writedlm("temp_data/β1max_$(run).csv",β1[2,:],',')
+# =#
 
+PyPlot.plot(αs,β0[1,:],"o",color="C0")
+PyPlot.plot(αs,β0[2,:],"o",color="C0")
+PyPlot.plot(αs,β1[1,:],"o",color="C1")
+PyPlot.plot(αs,β1[2,:],"o",color="C1")
 
