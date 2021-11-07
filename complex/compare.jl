29b8e27f67be5ead3ed92a25837c06b26d64de29
r0b1nu5 <robindelabays@ucsb.edu>
2021-11-06 21:49:09 -0700

    t,z,x

--- complex/compare.jl
+++ complex/compare.jl
@@ -9,22 +9,25 @@ B,w = L2B(L)
 A = diagm(0 => diag(L)) - L
 γ = 2/π
 
-θ0 = 2π*rand(n) .- π
-θ0 = 2π*(1:n)./n .- π + .1*rand(n)
+#θ0 = 2π*(1:n)./n .- π .- π/10 + .3*(2*rand(n).-1)
 θs = copy(θ0)
 θ = copy(θ0)
+
+z0 = copy(θ0) .+ im*0.
+zs = copy(z0)
+z = copy(z0)
 
 x0 = exp.(im*θ0)
 xs = copy(x0)
 x = copy(x0)
 
-T = 100000
+T = 50000
 h = .001
 t = 0
 c = 0
 
 while t < T
-	global θ,θs,x,xs,c
+	global θ,θs,z,zs,x,xs,c
 	global t += 1
 
 	if t%1000 == 0
@@ -32,6 +35,9 @@ while t < T
 		c += 1
 		writedlm("temp/th_$c.csv",θs[:,1:end-1],',')
 		θs = θs[:,end]
+		writedlm("temp/zr_$c.csv",real.(zs[:,1:end-1]),',')
+		writedlm("temp/zi_$c.csv",imag.(zs[:,1:end-1]),',')
+		zs = zs[:,end]
 		writedlm("temp/xr_$c.csv",real.(xs[:,1:end-1]),',')
 		writedlm("temp/xi_$c.csv",imag.(xs[:,1:end-1]),',')
 		xs = xs[:,end]
@@ -45,6 +51,14 @@ while t < T
 	θ += h*(k1 + 2*k2 + 2*k3 + k4)/6
 	θs = [θs (mod.(θ .+ π,2π) .- π)]
 
+	k1 = -γ*B*(sin.(B'*z) - im*cos.(B'*z))
+	k2 = -γ*B*(sin.(B'*(z + h*k1/2)) - im*cos.(B'*(z + h*k1/2)))
+	k1 = -γ*B*(sin.(B'*(z + h*k2/2)) - im*cos.(B'*(z + h*k2/2)))
+	k1 = -γ*B*(sin.(B'*(z + h*k3)) - im*cos.(B'*(z + h*k3)))
+
+	z += h*(k1 + 2*k2 + 2*k3 + k4)/6
+	zs = [zs z]
+	
 	k1 = γ*A*x
 	k2 = γ*A*(x + h*k1/2)
 	k3 = γ*A*(x + h*k2/2)
@@ -55,23 +69,43 @@ while t < T
 end
 
 Θs = Matrix{Float64}(undef,n,0)
+Zs = Matrix{Complex{Float64}}(undef,n,0)
 Xs = Matrix{Complex{Float64}}(undef,n,0)
 for d in 1:c
-	global Θs,Xs
+	global Θs,Zs,Xs
 	Θs = [Θs readdlm("temp/th_$d.csv",',')]
 	rm("temp/th_$d.csv")
+	Zs = [Zs (readdlm("temp/zr_$d.csv",',') + im*readdlm("temp/zi_$d.csv",','))]
+	rm("temp/zr_$d.csv")
+	rm("temp/zi_$d.csv")
 	Xs = [Xs (readdlm("temp/xr_$d.csv",',') + im*readdlm("temp/xi_$d.csv",','))]
 	rm("temp/xr_$d.csv")
 	rm("temp/xi_$d.csv")
 end
 Θs = [Θs θs]
+Zs = [Zs zs]
 Xs = [Xs xs]
 
 for i in 1:n
-	subplot(1,2,1)
+	subplot(1,3,1)
 	PyPlot.plot((0:T)*h,Θs[i,:])
-	subplot(1,2,2)
+	subplot(1,3,2)
+	PyPlot.plot((0:T)*h,real.(Zs[i,:]))
+	subplot(1,3,3)
 	PyPlot.plot((0:T)*h,angle.(Xs[i,:]))
 end
+
+subplot(1,3,1)
+title("Original Kuramoto")
+xlabel("t")
+ylabel("θ")
+subplot(1,3,2)
+title("Complex Kuramoto - 1st")
+xlabel("t")
+ylabel("real(z)")
+subplot(1,3,3)
+title("Complex Kuramoto - 2nd")
+xlabel("t")
+ylabel("arg(x)")
 
 
