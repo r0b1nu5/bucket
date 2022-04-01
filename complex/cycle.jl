e36e0b5a45826d110b7a8d2b5d48add1e6d5d3ad
r0b1nu5 <robindelabays@ucsb.edu>
2022-03-31 18:27:33 -0700

    nearest-neighbor cycle

--- complex/cycle.jl
+++ complex/cycle.jl
@@ -1,11 +1,21 @@
 using LinearAlgebra, SparseArrays
 
-function cycle(n::Int64)
-	return L = diagm(0 => 2*ones(n)) - diagm(1 => ones(n-1)) - diagm(-1 => ones(n-1)) - diagm(n-1 => ones(1)) - diagm(1-n => ones(1))
+function cycle(n::Int64, k::Int64=1)
+	A = zeros(n,n)
+	for i in 1:k
+		A += diagm(i => ones(n-i)) + diagm(-i => ones(n-i)) + diagm(n-i => ones(i)) + diagm(i-n => ones(i))
+	end
+
+	return diagm(0 => 2*k*ones(n)) - A
 end
 
 function sp_cycle(n::Int64)
-	return L = spdiagm(0 => 2*ones(n)) - spdiagm(1 => ones(n-1)) - spdiagm(-1 => ones(n-1)) - spdiagm(n-1 => ones(1)) - spdiagm(1-n => ones(1))
+	A = spzeros(n,n)
+	for i in 1:k
+		A += spdiagm(i => ones(n-i)) + spdiagm(-i => ones(n-i)) + spdiagm(n-i => ones(i)) + spdiagm(i-n => ones(i))
+	end
+
+	return spdiagm(0 => 2*k*ones(n)) - A
 end
 
 
