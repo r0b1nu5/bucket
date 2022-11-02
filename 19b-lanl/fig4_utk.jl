be21f0d3002f597f55da7041e01a9db2f61761a6
r0b1nu5 <robindelabays@ucsb.edu>
2022-11-02 22:31:06 +0100

    show neighbors

--- 19b-lanl/fig4_utk.jl
+++ 19b-lanl/fig4_utk.jl
@@ -9,7 +9,7 @@ colshift2 = .5
 cols = [cmap(1-(i+colshift1)/(2+colshift1+colshift2)) for i in 0:2]
 # =#
 # #=
-cols = [(243,111,33)./255,(0,145,194)./255,(161,0,202)./255]
+cols = [(243,111,33)./255,(0,145,194)./255,(161,0,202)./255,"C2"]
 # =#
 gra = (.8,.8,.8,1.)
 
@@ -24,7 +24,12 @@ file = "mysterious_forcingDrift"
 id0 = Int64.(vec(readdlm("data_utk/utk2_ids.csv",',')))
 fs1 = 51
 fs1id = id0[fs1]
-#fs1 = 66
+fs2 = 66
+fs2id = id0[fs2]
+fs3 = 27
+fs3id = id0[fs3]
+fs4 = 14
+fs4id = id0[fs4]
 
 # #=
 L0 = zeros(length(ls),length(ks))
@@ -70,6 +75,9 @@ ids = (1:size(xy)[1]).*(xy[:,1] .< -55.)
 ids = setdiff(ids,[0,])
 PyPlot.plot(xy[ids,1],xy[ids,2],"ok",markersize=5.)
 PyPlot.plot(xy[fs1,1],xy[fs1,2],"o",color=cols[1],markersize=10.)
+PyPlot.plot(xy[fs2,1],xy[fs2,2],"o",color=cols[1],markersize=10.)
+PyPlot.plot(xy[fs3,1],xy[fs3,2],"o",color=cols[2],markersize=10.)
+PyPlot.plot(xy[fs4,1],xy[fs4,2],"o",color=cols[3],markersize=10.)
 axis([-105.,-63.,24.,48.])
 PyPlot.xticks([])
 PyPlot.yticks([])
@@ -77,11 +85,15 @@ PyPlot.yticks([])
 
 subplot2grid((2,6),(0,3),colspan=3,rowspan=2)
 
-L0red = nL0[[1:fs1-1;fs1+1:size(nL0)[1]],:]
+#L0red = nL0[[1:fs1-1;fs1+1:size(nL0)[1]],:]
+L0red = nL0[[1:fs4-1;fs4+1:fs3-1;fs3+1:fs1-1;fs1+1:fs2-1;fs2+1:size(nL0)[1]],:]
 L0max = [maximum(L0red[:,i]) for i in 1:size(L0red)[2]]
 L0min = [minimum(L0red[:,i]) for i in 1:size(L0red)[2]]
 PyPlot.fill([ks;ks[end:-1:1]]/T,-[L0max;L0min[end:-1:1]],color=gra)
-PyPlot.plot(ks/T,-nL0[fs1,:],"-",color=cols[1],linewidth=2.,label="$fs1id")
+PyPlot.plot(ks/T,-nL0[fs1,:],"-",color=(0.,0.,0.),linewidth=2.,label="$fs1id")
+PyPlot.plot(ks/T,-nL0[fs2,:],"-",color=cols[1],linewidth=2.,label="$fs2id")
+PyPlot.plot(ks/T,-nL0[fs3,:],"-",color=cols[2],linewidth=2.,label="$fs3id")
+PyPlot.plot(ks/T,-nL0[fs4,:],"-",color=cols[3],linewidth=2.,label="$fs4id")
 axis([0.,maximum(ks)/T,-.1,1.1])
 xlabel("freq")
 ylabel("normalized log-likelihodd")
