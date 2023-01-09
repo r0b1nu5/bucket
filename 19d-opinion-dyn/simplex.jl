b6fe1b5dcb7a4287fdaa4eb4f42a1ce6bee9408c
r0b1nu5 <robindelabays@ucsb.edu>
2023-01-09 08:50:26 +0100

    sliding summits

--- 19d-opinion-dyn/simplex.jl
+++ 19d-opinion-dyn/simplex.jl
@@ -67,8 +67,82 @@ function unif_simplex_arb(S::Matrix{Float64})
 	return x
 end
 
+# Slides the point x (typically in the simplex) towards the centroid of the unitary p-simplex. 
+# For α = 0, the summit does not move, and for α = 1, the summit reaches the centroid.
 
+function slide_summit(x::Vector{Float64}, α::Float64)
+	if α < 0.
+		@info "Warning: α < 0."
+	elseif α > 1.
+		@info "Warning: α > 1."
+	end
 
+	p = length(x)
+	c = 1/p*ones(p)
+
+	return (1-α)*x + α*p
+end
+
+# Slides each column of xs towards the centroid of the unitary p-simplex by a factor α. 
+
+function slide_summit(xs::Matrix{Float64}, α::Float64)
+	p,n = size(xs)
+
+	S = zeros(p,0)
+
+	for i in 1:n
+		x = xs[:,i]
+		S = [S slide_summit(x,α)]
+	end
+
+	return S
+end
+
+# Slides the i-th column of xs towards the centroid of the unitary p-simplex by a factor αs[i]. 
+
+function slide_summit(xs::Matrix{Float64}, αs::Vector{Float64})
+	p,n = size(xs)
+	S = zeros(p,0)
+
+	for i in 1:n
+		S = [S slide_summit(xs[:,i],αs[i])]
+	end
+
+	return S
+end
+
+# Slides the q-th summit of the unitary p-simplex towards its centroid by a factor α. 
+
+function slide_summit(p::Int64, q::Int64, α::Float64)
+
+	s = [zeros(q-1);1.;zeros(p-q)]
+	
+	return slide_summit(s,α)
+end
+
+# Slides each of the qs summits towards the centroid by a factor α.
+
+function slide_summit(p::Int64, qs::Vector{Int64}, α::Float64)
+	S = zeros(p,0)
+
+	for q in qs
+		S = [S slide_summit(p,q,α)]
+	end
+
+	return S
+end
+
+# Slides each of the qs summits towards the centroid by a factor αs[i] respectively.
+
+function slide_summit(p::Int64, qs::Vector{Int64}, αs::Vector{Float64})
+	S = zeros(p,0)
+
+	for i in 1:length(qs)
+		S = [S slide_summit(p,qs[i],αs[i])]
+	end
+
+	return S
+end
 
 
 
