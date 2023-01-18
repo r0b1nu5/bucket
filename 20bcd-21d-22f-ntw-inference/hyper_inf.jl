705569286c13848a6468df6573153f76ed7da118
r0b1nu5 <robindelabays@ucsb.edu>
2023-01-18 19:08:06 +0100

    test the inference...

--- 20bcd-21d-22f-ntw-inference/hyper_inf.jl
+++ 20bcd-21d-22f-ntw-inference/hyper_inf.jl
@@ -84,72 +84,79 @@ function check_inference(A2::Matrix{Float64}, A3::Array{Float64,3}, inf_o::Dict{
     A2t = zeros(n,n)
     A3t = zeros(n,n,n)
 
-    e2 = [0 1;1 0.]
-    for id in inf_o[2]
-        A2t[id,id] = e2
+    if 2 in keys(inf_o)
+	    e2 = [0 1;1 0.]
+	    for id in inf_o[2]
+	        A2t[id,id] = e2
+	    end
+	
+	    p2 = 0
+	    fp2 = 0
+	    n2 = 0
+	    fn2 = 0
+	    for i in 1:n-1
+	        for j in i+1:n
+	            if A2t[i,j] > .1
+	                p2 += 1
+	                if A2[i,j] < .1
+	                    fp2 += 1
+	                end
+	            else
+	                n2 += 1
+	                if A2[i,j] > .1
+	                    fn2 += 1
+	                end
+	            end
+	        end
+	    end
+	    tp2 = p2 - fp2
+	    tn2 = n2 - fn2
+	
+	    sen2 = tp2/(tp2 + fn2)
+	    spe2 = tn2/(tn2 + fp2)
+	
+	    @info "Detection of 2-edges: sensitivity = $sen2, specificity = $spe2."
+    else
+	    (sen2,spe2,tp2,fp2,tn2,fn2) = (NaN,NaN,NaN,NaN,NaN,NaN)
     end
-
-    e3 = zeros(3,3,3)
-    e3[1,2,3] = e3[1,3,2] = e3[2,1,3] = e3[2,3,1] = e3[3,1,2] = e3[3,2,1] = 1.
-    for id in inf_o[3]
-        A3t[id,id,id] = e3
+    if 3 in keys(inf_o)
+	    e3 = zeros(3,3,3)
+	    e3[1,2,3] = e3[1,3,2] = e3[2,1,3] = e3[2,3,1] = e3[3,1,2] = e3[3,2,1] = 1.
+	    for id in inf_o[3]
+	        A3t[id,id,id] = e3
+	    end
+	    
+	    p3 = 0
+	    fp3 = 0
+	    n3 = 0
+	    fn3 = 0
+	    for i in 1:n-2
+	        for j in i+1:n-1
+	            for k in j+1:n
+	                if A3t[i,j,k] > .1
+	                    p3 += 1
+	                    if A3[i,j,k] < .1
+	                        fp3 += 1
+	                    end
+	                else
+	                    n3 += 1
+	                    if A3[i,j,k] > .1
+	                        fn3 += 1
+	                    end
+	                end
+	            end
+	        end
+	    end
+	    tp3 = p3 - fp3
+	    tn3 = p3 - fn3
+	
+	    sen3 = tp3/(tp3 + fn3)
+	    spe3 = tn3/(tn3 + fp3)
+	
+	    @info "Detection of 3-edges: sensitivity = $sen3, specificity = $spe3."
+    else
+	    (sen3,spe3,tp3,fp3,tn3,fn3) = (NaN,NaN,NaN,NaN,NaN,NaN)
     end
-
-    p2 = 0
-    fp2 = 0
-    n2 = 0
-    fn2 = 0
-    for i in 1:n-1
-        for j in i+1:n
-            if A2t[i,j] > .1
-                p2 += 1
-                if A2[i,j] < .1
-                    fp2 += 1
-                end
-            else
-                n2 += 1
-                if A2[i,j] > .1
-                    fn2 += 1
-                end
-            end
-        end
-    end
-    tp2 = p2 - fp2
-    tn2 = n2 - fn2
-
-    sen2 = tp2/(tp2 + fn2)
-    spe2 = tn2/(tn2 + fp2)
-
-    @info "Detection of 2-edges: sensitivity = $sen2, specificity = $spe2."
-
-    p3 = 0
-    fp3 = 0
-    n3 = 0
-    fn3 = 0
-    for i in 1:n-2
-        for j in i+1:n-1
-            for k in j+1:n
-                if A3t[i,j,k] > .1
-                    p3 += 1
-                    if A3[i,j,k] < .1
-                        fp3 += 1
-                    end
-                else
-                    n3 += 1
-                    if A3[i,j,k] > .1
-                        fn3 += 1
-                    end
-                end
-            end
-        end
-    end
-    tp3 = p3 - fp3
-    tn3 = p3 - fn3
-
-    sen3 = tp3/(tp3 + fn3)
-    spe3 = tn3/(tn3 + fp3)
-
-    @info "Detection of 3-edges: sensitivity = $sen3, specificity = $spe3."
 
     return (sen2,spe2,tp2,fp2,tn2,fn2), (sen3,spe3,tp3,fp3,tn3,fn3)
 end
---
+++ 20bcd-21d-22f-ntw-inference/test_hyper_inf.jl
@@ -0,0 +1,90 @@
+include("hyper_inf.jl")
+include("hyper_kuramoto.jl")
+include("gen_rand_hyperg.jl")
+
+n = 7
+p1 = .5
+p2 = .2 
+p3 = .2
+
+A2,A3 = gen_rand_hyperwheel(n,p1,p2,p3)
+
+ #=
+# Testing the efficiency of the inference using the result of the vector field directly.
+
+X = .2*rand(n,100) .- .1
+Y = f_kuramoto_3rd(X,A2,A3,zeros(n))
+
+sens = Float64[]
+spes = Float64[]
+iters = 10:5:100
+for iter in iters
+	xxx = hyper_inf(X[:,1:iter],Y[:,1:iter],[3,],4)
+	yyy = check_inference(A2,A3,xxx[1])
+	push!(sens,yyy[2][1])
+	push!(spes,yyy[2][2])
+end
+
+figure("Perfect measurement")
+PyPlot.plot(iters,sens,"-o",label="sensitivity")
+PyPlot.plot(iters,spes,"-o",label="specificity")
+xlabel("Number of measurements")
+legend()
+# =#
+
+# #=
+# Testing the efficiency of the inference using the actual time step (extracted from RK4 integration).
+
+X = zeros(n,0)
+Y = zeros(n,0)
+
+for i in 1:200
+	x,dx = hyper_k(A2,A3,zeros(n),.2*rand(n) .- .1,.1,.0,.001,100)
+	global X = [X x]
+	global Y = [Y dx]
+end
+
+ #=
+Xt = X[:,1:100:20000]
+Yt = Y[:,1:100:20000]
+sens = Float64[]
+spes = Float64[]
+iters = 10:20:200
+for iter in iters
+	xxx = hyper_inf(Xt[:,1:iter],Yt[:,1:iter],[3,],4)
+	yyy = check_inference(A2,A3,xxx[1])
+	push!(sens,yyy[2][1])
+	push!(spes,yyy[2][2])
+end
+
+figure("Actual variation (RK4 measurement)")
+PyPlot.plot(iters,sens,"-o",label="sensitivity")
+PyPlot.plot(iters,spes,"-o",label="specificity")
+xlabel("Number of time steps of length 1")
+legend()
+# =#
+
+# #=
+idx = sort([1:100:20000;2:100:20000])
+Xt = X[:,idx]
+Yt = Y[:,idx]
+sens = Float64[]
+spes = Float64[]
+iters = 10:20:400
+for iter in iters
+	xxx = hyper_inf(Xt[:,1:iter],Yt[:,1:iter],[3,],4)
+	yyy = check_inference(A2,A3,xxx[1])
+	push!(sens,yyy[2][1])
+	push!(spes,yyy[2][2])
+end
+
+figure("Actual variation (RK4 measurement)")
+PyPlot.plot(iters./2,sens,"-o",label="sensitivity")
+PyPlot.plot(iters./2,spes,"-o",label="specificity")
+xlabel("Number of time steps of length 2")
+legend()
+# =#
+
+
+
+
