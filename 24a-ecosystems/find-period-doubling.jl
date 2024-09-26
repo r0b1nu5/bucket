8bb947b53a1929bf04824e97761a46a7d62931c2
r0b1nu5 <robindelabays@ucsb.edu>
2024-09-26 22:59:06 +0200

    looking for period doubling...

--- 24a-ecosystems/find-period-doubling.jl
+++ 24a-ecosystems/find-period-doubling.jl
@@ -7,14 +7,15 @@ S = 21
 Si = 157
 A = readdlm("data/ex-limit-cycle-04-A.csv",',')
 N0 = vec(readdlm("data/ex-limit-cycle-04-N0.csv",','))
+N0 = vec(readdlm("data/ex-limit-cycle-04-s134-N0.csv",','))
 
 zer0 = 1e-15
 
 κ = 1.
 μ = 5.
 σ = 2.7
-nσ = 20
-σs = LinRange(2.7,2.8,nσ)
+nσ = 191
+σs = LinRange(2.7,3.7,nσ)
 Id = diagm(0 => ones(S))
 
 #N0 = inv(Id + μ/Si*ones(S,S) + σ/sqrt(Si)*A)*κ*ones(S)
@@ -24,10 +25,12 @@ Id = diagm(0 => ones(S))
 n_iter = 2_000_000
 δt = 1e-3
 
-for i in 1:nσ
+for i in 134:nσ
 	σ = σs[i]
+	@info "σ = $σ/$(maximum(σs))"
 	N = lv_bunin(N0,A,κ,μ/Si,σ/sqrt(Si),n_iter,n_iter,δt)
 	F = fft(N[:,100_001:n_iter])
+	writedlm("data/ex-limit-cycle-04-s$i-N0.csv",N0,',')
 	writedlm("data/ex-limit-cycle-04-s$i-fft.csv",abs.(F[:,1:200]),',')
 	global N0 = N[:,end]
 end
--- 24a-ecosystems/plot-period-doubling.jl
+++ 24a-ecosystems/plot-period-doubling.jl
@@ -1,23 +1,35 @@
-using PyPlot
+using PyPlot, DelimitedFiles
 
-nσ = 20
-σs = LinRange(2.7,2.8,nσ)
+nσ = 191
+σs = LinRange(2.7,3.7,nσ)
 
 T = 2_000_000 - 100_000
 δt = 1e-3
 ωs = (0:T-1)*2π/(T*δt)
 F = zeros(0,199)
 
+#nσ = 61
+#σs = σs[1:61]
+
 for i in 1:nσ
-	global F = vcat(F,readdlm("data/ex-limit-cycle-04-s$i-fft.csv",',')[[1,],2:200])
+	f = readdlm("data/ex-limit-cycle-04-s$i-fft.csv",',')[[1,],2:200]
+	global F = vcat(F,f./maximum(f))
 end
 
-figure()
+figure("Fourier",(12,4))
 subplot(2,1,1)
-PyPlot.contourf(ωs[2:200],σs,F)
+PyPlot.contourf(ωs[2:200],σs,F,50)
+ylabel("σ")
+colorbar(label="|fft|")
 subplot(2,1,2)
-PyPlot.contourf(ωs[2:200],σs,log.(F))
+PyPlot.contourf(ωs[2:200],σs,log.(F),50)
+xlabel("ω [rad/s]")
+ylabel("σ")
+colorbar(label="log|fft|")
 
+figure()
+PyPlot.surf(ωs[2:200],σs,F,cmap=get_cmap("viridis"))
+#=
 PyPlot.matshow(F[nσ:-1:1,2:151])
 xticks(0:5:149,round.(ωs[2:5:151],digits=2))
 xlabel("ω")
@@ -29,4 +41,5 @@ xticks(0:5:99,round.(ωs[2:5:101],digits=2))
 xlabel("ω")
 yticks([0,nσ-1],[2.8,2.7])
 ylabel("σ")
+=#
 
