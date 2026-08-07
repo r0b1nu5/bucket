aaf8338a60b6e0bb282c95bec60d4ebc365566fe
r0b1nu5 <robindelabays@ucsb.edu>
2026-08-07 15:49:24 +0200

    last update

--- 25g-hypergraph-emergence/emergence-sqlat.jl
+++ 25g-hypergraph-emergence/emergence-sqlat.jl
@@ -25,10 +25,16 @@ amplitude = 1.
 X = amplitude*(rand(n^2,T) .- .5)
 Y = f_kuramoto(X,zeros(n^2),B,1.,π/4)
 
+ #= # Normalizing the states
+for i in 1:size(X)[1]
+	X[i,:] = X[i,:]./mean(X[i,:])
+end
+# =#
+
 #################### INFERENCE ###################################
 
-ooi = [2,3]
-dmax = 2
+ooi = [2,3,4]
+dmax = 3
 zer0 = 1e-4
 λ = 0.1
 
@@ -41,11 +47,21 @@ k2Ainf = Dict{Int64,Any}(0 => Ainf)
 
 m2max = 1.
 m3max = 1.
+m4max = 1.
 #m2max = (n*(n-1))
 #m3max = (n*(n-1)*(n-2))
 m2 = [sum(abs.(Ainf[2][:,3]) .> zer0)/m2max,]
 m3 = [sum(abs.(Ainf[3][:,4]) .> zer0)/m3max,]
+m3 = [sum(abs.(Ainf[4][:,5]) .> zer0)/m4max,]
 m2true = [2*(n-1)^2+2*(n-1),]
+
+list_3edges = Vector{Vector{Tuple{Int64,Int64,Int64}}}()
+push!(list_3edges,Tuple{Int64,Int64,Int64}[])
+for i in 1:size(Ainf[3])[1]
+	if abs(Ainf[3][i,4]) > zer0
+		push!(list_3edges[1],(Ainf[3][i,1],Ainf[3][i,2],Ainf[3][i,3]))
+	end
+end
 
 # Contribution of 2-edges to the dynamics
 global mag2 = zeros(size(X)[2])
@@ -62,6 +78,14 @@ for i in 1:size(Ainf[3])[1]
 	global mag3 += abs.(Ainf[3][i,4]*(X[a,:].*X[b,:]))
 end
 contribution3 = [median(mag3),]
+global mag4 = zeros(size(X)[2])
+for i in 1:size(Ainf[4])[1]
+	a = Int64(Ainf[4][i,2])
+	b = Int64(Ainf[4][i,3])
+	c = Int64(Ainf[4][i,4])
+	global mag4 += abs.(Ainf[4][i,5]*(X[a,:].*X[b,:].*X[c,:]))
+end
+contribution4 = [median(mag4),]
 
 distances = Vector{Int64}[]
 
@@ -85,6 +109,13 @@ for k in ks
 	push!(m3,sum(abs.(Ainf2[3][:,4]) .> zer0))
 	#push!(m2,sum(abs.(Ainf2[2][:,3]) .> zer0)/(nc*(nc-1)))
 	#push!(m3,sum(abs.(Ainf2[3][:,4]) .> zer0)/(nc*(nc-1)*(nc-2)))
+	
+	push!(list_3edges,Tuple{Int64,Int64,Int64}[])
+	for i in 1:size(Ainf2[3])[1]
+		if abs(Ainf2[3][i,4]) > zer0
+			push!(list_3edges[end],(Ainf2[3][i,1],Ainf2[3][i,2],Ainf2[3][i,3]))
+		end
+	end
 
 	# Contribution of 2-edges to the dynamics
 	mag2 = zeros(size(X2)[2])
@@ -101,7 +132,14 @@ for k in ks
 		mag3 += abs.(Ainf2[3][i,4]*(X2[a,:].*X2[b,:]))
 	end
 	push!(contribution3,median(mag3))
-	
+	mag4 = zeros(size(X)[2])
+	for i in 1:size(Ainf2[4])[1]
+		a = Int64(Ainf2[4][i,2])
+		b = Int64(Ainf2[4][i,3])
+		c = Int64(Ainf2[4][i,4])
+		mag4 += abs.(Ainf[4][i,5]*(X[a,:].*X[b,:].*X[c,:]))
+	end
+	push!(contribution4,median(mag4))
 end
 
 
@@ -145,104 +183,8 @@ tight_layout()
 show()
 
 
-# =#
 
 
- #=
-c = 0
-for iter in iters
-	global c += 1
-	@info "Run $c/$(length(iters))"
-
-	# Run THIS
-	xxx = hyper_inf(X[:,1:iter],Y[:,1:iter],ooi,dmax,1e-1)
-	A2us = xxx[1][2] # Pairwise interactions
-	A3us = xxx[1][3] # Triadic interactions
-
-	if save
-		writedlm("data/kuramoto-"*ntw*"-n$n-iter$iter-A2.csv",A2,',')
-		writedlm("data/kuramoto-"*ntw*"-n$n-iter$iter-A3.csv",A3,',')
-		writedlm("data/kuramoto-"*ntw*"-n$n-iter$iter-A2this.csv",A2us,',')
-		writedlm("data/kuramoto-"*ntw*"-n$n-iter$iter-A3this.csv",A3us,',')
-	end
- #=
-	A2us_ = zeros(n,n)
-	for l in 1:size(A2us)[1]
-		i = Int64(A2us[l,1])
-		j = Int64(A2us[l,2])
-		A2us_[i,j] = A2us[l,3]
-	end
-# =#
- #=
-	A3us_ = zeros(n,n,n)
-	for l in 1:size(A3us)[1]
-		i = Int64(A3us[l,1])
-		j = Int64(A3us[l,2])
-		k = Int64(A3us[l,3])
-		A3us_[i,j,k] = A3us[l,4]
-		A3us_[i,k,j] = A3us[l,4]
-	end
-	adjus_ = get_adj_3rd(A2us_,A3us_)[1]
-	adju_ = cat_As(A2us_,A3us_)
-# =#
- #=
-	A2us_ = inferred_adj_2nd(xxx[2][2],n)[2]
-	A3us_ = inferred_adj_3rd(xxx[2][3],n)[2]
-	adjus_ = get_adj_3rd(A2us_,A3us_)[1]
-	adju_ = cat_As(A2us_,A3us_)
-# =#
-@info "============= WE ARE DONE: $(now()) ================"
-
-	ξ = 1e-10
-
-	tpr,fpr = my_ROC(abs.(A2us),A2l,abs.(A3us),A3l,n)
-	tpr2,fpr2 = my_ROC(abs.(A2us),A2l,n)
-	tpr3,fpr3 = my_ROC(abs.(A3us),A3l,n)
-
-	cmapme = get_cmap("RdPu")
-	
-	figure("ROCs-"*ntw*"-$n",(13.5,4))
-	subplot(1,3,1)
-	PyPlot.plot(fpr,tpr,color=cmapme((iter-minimum(iters))/max(1,(maximum(iters)-minimum(iters)))))
-	subplot(1,3,2)
-	PyPlot.plot(fpr2,tpr2,color=cmapme((iter-minimum(iters))/max(1,(maximum(iters)-minimum(iters)))))
-	subplot(1,3,3)
-	PyPlot.plot(fpr3,tpr3,color=cmapme((iter-minimum(iters))/max(1,(maximum(iters)-minimum(iters)))))
-
- #=
-	rocadjus = roc(abs.(adju_) + ξ*rand(Float64,size(adju_)),adj)
-	subplot(1,3,1)
-	PyPlot.plot(rocadjus.FPR,rocadjus.TPR,"k")
-# =# 
- #=
-	rocA2us = roc(abs.(A2us_) + ξ*rand(n,n),A2)
-	subplot(1,3,2)
-	PyPlot.plot(rocA2us.FPR,rocA2us.TPR,":k",alpha=1-.5*((iter-minimum(iters))/max(1,maximum(iters)-minimum(iters))))
-# =#
-#=
-	rocA3us = roc(abs.(A3us_) + ξ*rand(n,n,n),A3)
-	subplot(1,3,3)
-	PyPlot.plot(rocA3us.FPR,rocA3us.TPR,":k",alpha=1-.5*((iter-minimum(iters))/max(1,maximum(iters)-minimum(iters))))
-# =#
-end
-
-
-
-figure("ROCs-"*ntw*"-$n",)
-subplot(1,3,1)
-xlabel("FPR")
-ylabel("TPR")
-title("THIS")
-subplot(1,3,2)
-xlabel("FPR")
-title("THIS, pairwise")
-subplot(1,3,3)
-title("THIS, triadic")
-xlabel("FPR")
-
-@info "############# FINISHED: $(now())"
-# =#
-#end
 
 
 
